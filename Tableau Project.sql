d3c814716651765c8c493f124cddbbb326a227ae
tuleminh1992 <63758702+tuleminh1992@users.noreply.github.com>
2021-12-12 01:04:22 -0600

    Update Tableau Project.sql

--- Tableau Project.sql
+++ Tableau Project.sql
@@ -1,44 +1,46 @@
-# Task 1:
-select
+--- Task 1: Create a visualization that provides a breakdown between the male and female employees working in the company each year, starting from 1990. 
+
+Select
 	year(d.from_date) as calendar_year,
-    e.gender as Gender,
-    count(e.emp_no) as number_of_employees
+   	e.gender as Gender,
+    	count(e.emp_no) as number_of_employees
 from
 	t_employees e
 join
 	t_dept_emp d
-on e.emp_no = d.emp_no 
-where year(d.from_date) >= 1990
+on 	e.emp_no = d.emp_no 
+where 	year(d.from_date) >= 1990
 GROUP BY year(d.from_date), gender;
 
-# Task 2:
+--- Task 2: Compare the number of male managers to the number of female managers from different departments for each year, starting from 1990.
+
 select 
 	d.dept_name ,
-    ee.gender ,
+    	ee.gender ,
 	dm.emp_no,
 	dm.from_date,
-    dm.to_date,
-    e.calendar_year,
-    case 
+    	dm.to_date,
+    	e.calendar_year,
+    	case 
 		when e.calendar_year between year(dm.from_date) and year(dm.to_date) then 1
         else 0
 	END as active
 from 
 	(select 
 		year(hire_date) as calendar_year 
-	 from t_employees
-     GROUP BY calendar_year) e
-	 cross join t_dept_manager dm
-     join t_departments d on dm.dept_no  = d.dept_no
-     join t_employees ee on dm.emp_no = ee.emp_no
+	 from 	t_employees
+     	 GROUP BY calendar_year) e
+cross join t_dept_manager dm
+join t_departments d on dm.dept_no  = d.dept_no
+join t_employees ee on dm.emp_no = ee.emp_no
 order by dm.emp_no, calendar_year;     
 
-# Task 3:
+--- Task 3: Compare the average salary of female versus male employees in the entire company until year 2002, and add a filter allowing you to see that per each department.
 Select 
 	round(avg(s.salary),2) as average_salary,
 	e.gender as gender,
 	d.dept_name as departments,
-    year(s.from_date) as calendar_year
+   	year(s.from_date) as calendar_year
 from
 	t_salaries s
     join t_employees e on s.emp_no = e.emp_no
@@ -48,7 +50,9 @@ group by   d.dept_no, e.gender, calendar_year
 having calendar_year <=2002
 order by d.dept_no;
 
-# Task 4:
+--- Task 4: Create an SQL stored procedure that will allow you to obtain the average male and female salary per department within a certain salary range. 
+--- Let this range be defined by two values the user can insert when calling the procedure.
+--- Finally, visualize the obtained result-set in Tableau as a double bar chart.
 delimiter $$
 create PROCEDURE filter_salary( IN p_salary_min float, IN p_salary_max float)
 begin
