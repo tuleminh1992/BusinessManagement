e8a4bc796a2da92a990fc1aa0b5fcd190811ca47
Tu Minh Le <63758702+tuleminh1992@users.noreply.github.com>
2021-12-12 20:15:22 -0600

    Update Tableau Project.sql

--- Tableau Project.sql
+++ Tableau Project.sql
@@ -24,12 +24,12 @@ select
     	case 
 		when e.calendar_year between year(dm.from_date) and year(dm.to_date) then 1
         else 0
-	END as active
+	END as active --- CASE Statement to specialize ACTIVE Manager by the calendar year is in between start date and end date  
 from 
 	(select 
 		year(hire_date) as calendar_year 
 	 from 	t_employees
-     	 GROUP BY calendar_year) e
+     	 GROUP BY calendar_year) e --- Create a column contains the year from the higher date of employee table must cross join the Department Table
 cross join t_dept_manager dm
 join t_departments d on dm.dept_no  = d.dept_no
 join t_employees ee on dm.emp_no = ee.emp_no
@@ -54,14 +54,14 @@ order by d.dept_no;
 --- Let this range be defined by two values the user can insert when calling the procedure.
 --- Finally, visualize the obtained result-set in Tableau as a double bar chart.
 delimiter $$
-create PROCEDURE filter_salary( IN p_salary_min float, IN p_salary_max float)
+create PROCEDURE filter_salary( IN p_salary_min float, IN p_salary_max float) --- Create 2 input Value  in a PROCEDURE
 begin
 	select e.gender, d.dept_name, avg(s.salary) as average_salary
     from t_salaries s
-	join t_employees e on s.emp_no = e.emp_no
+	join t_employees e on s.emp_no = e.emp_no --- JOIN Table together
     join t_dept_emp de on e.emp_no = de.emp_no
     join t_departments d on d.dept_no = de.dept_no
-	where s.salary BETWEEN p_salary_min and p_salary_max
+	where s.salary BETWEEN p_salary_min and p_salary_max --- WHERE condition stating the salary range from 2 input VALUE
 group by d.dept_no, e.gender;
 end$$
 
